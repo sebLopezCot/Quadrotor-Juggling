dc9fc94a73c78f52dfdbbed63a0f157c48c22fe1
Sebastian Lopez-Cot <slcot@mit.edu>
2018-05-06 03:55:43 -0400

    ball v1

--- sim.py
+++ sim.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle, Circle
 
 class Quadrotor(object):
     
@@ -41,8 +41,11 @@ class Ball(object):
         self.g = -9.81
 
     def step_dynamics(self, t, state, contact):
+        if contact:
+            state[3] = -state[3]*0.9
+        
         state[0] += state[2]*t
-        state[1] += state[3]*t
+        state[1] += state[3]*t + 0.5*self.g*t**2.0
         state[2] += 0.0
         state[3] += self.g*t
 
@@ -50,9 +53,10 @@ class Ball(object):
 
 class Animator(object):
 
-    def __init__(self, t, states):
+    def __init__(self, t, quad_states, ball_states):
         self.t = t
-        self.states = states
+        self.quad_states = quad_states
+        self.ball_states = ball_states
         
         # first set up the figure, the axis, and the plot elements we want to animate
         fig = plt.figure()
@@ -62,8 +66,8 @@ class Animator(object):
         self.cart_height = 0.05
         
         # set the limits based on the motion
-        self.xmin = -15 #np.around(states[:, 0].min() - self.cart_width / 2.0, 1)
-        self.xmax = 15 #np.around(states[:, 0].max() + self.cart_width / 2.0, 1)
+        self.xmin = -5 #np.around(quad_states[:, 0].min() - self.cart_width / 2.0, 1)
+        self.xmax = 5 #np.around(quad_states[:, 0].max() + self.cart_width / 2.0, 1)
         
         # create the axes
         self.ax = plt.axes(xlim=(self.xmin, self.xmax), ylim=(-5, 5), aspect='equal')
@@ -72,15 +76,17 @@ class Animator(object):
         self.time_text = self.ax.text(0.04, 0.9, '', transform=self.ax.transAxes)
         
         # create a rectangular cart
-        self.rect = Rectangle([states[0, 0] - self.cart_width / 2.0, states[0,0]-self.cart_height / 2],
+        self.ball = Rectangle([ball_states[0,0], ball_states[0,1]], 0.1, 0.1)
+        self.rect = Rectangle([quad_states[0, 0] - self.cart_width / 2.0, quad_states[0,0]-self.cart_height / 2],
             self.cart_width, self.cart_height, fill=True, color='red', ec='black')
-        self.rect_2 = Rectangle([states[0,0] - self.cart_width / 2.0, states[0,0]-self.cart_height / 2],
+        self.rect_2 = Rectangle([quad_states[0,0] - self.cart_width / 2.0, quad_states[0,0]-self.cart_height / 2],
                 0.05, 0.05, fill=True, color='blue', ec='black')
-        self.rect_3 = Rectangle([states[0,0] + self.cart_width / 2.0 - 0.05, states[0,0]-self.cart_height / 2],
+        self.rect_3 = Rectangle([quad_states[0,0] + self.cart_width / 2.0 - 0.05, quad_states[0,0]-self.cart_height / 2],
                 0.05, 0.05, fill=True, color='blue', ec='black')
         self.ax.add_patch(self.rect)
         self.ax.add_patch(self.rect_2)
         self.ax.add_patch(self.rect_3)
+        self.ax.add_patch(self.ball)
 
         # call the animator function
         anim = animation.FuncAnimation(fig, self.animate, frames=len(t), init_func=self.init_anim,
@@ -97,38 +103,47 @@ class Animator(object):
         self.rect.set_xy((0.0, 0.0))
         self.rect_2.set_xy((0.0, 0.0))
         self.rect_3.set_xy((0.0, 0.0))
-        return self.time_text, self.rect, self.rect_2, self.rect_3, 
+        self.ball.set_xy((0.0, 0.0))
+        return self.time_text, self.rect, self.rect_2, self.rect_3, self.ball
 
     # animation function: update the objects
     def animate(self, i):
         self.time_text.set_text('time = {:2.2f}'.format(self.t[i]))
         
-        self.rect.set_xy((self.states[i, 0] - self.cart_width / 2.0, self.states[i,1]-self.cart_height / 2))
-        self.rect_2.set_xy((self.states[i,0] - self.cart_width / 2.0, self.states[i,1]))
-        self.rect_3.set_xy((self.states[i,0] + self.cart_width / 2.0 - 0.05, self.states[i,1]))
+        self.ball.set_xy((self.ball_states[i,0], self.ball_states[i,1]))
+        self.rect.set_xy((self.quad_states[i, 0] - self.cart_width / 2.0, self.quad_states[i,1]-self.cart_height / 2))
+        self.rect_2.set_xy((self.quad_states[i,0] - self.cart_width / 2.0, self.quad_states[i,1]))
+        self.rect_3.set_xy((self.quad_states[i,0] + self.cart_width / 2.0 - 0.05, self.quad_states[i,1]))
 
-        t = matplotlib.transforms.Affine2D().rotate_around(self.states[i, 0], self.states[i, 1], self.states[i, 2])
+        t = matplotlib.transforms.Affine2D().rotate_around(self.quad_states[i, 0], self.quad_states[i, 1], self.quad_states[i, 2])
 
         self.rect.set_transform(t + plt.gca().transData)
         self.rect_2.set_transform(t + plt.gca().transData)
         self.rect_3.set_transform(t + plt.gca().transData)
 
-        return self.time_text, self.rect, self.rect_2, self.rect_3, 
+        return self.time_text, self.rect, self.rect_2, self.rect_3, self.ball
 
 
 if __name__ == "__main__":
 
     quad = Quadrotor()
+    ball = Ball()
 
     dt = 0.1
     steps = 1000
 
     time = dt * np.arange(steps)
 
-    state = np.zeros((steps, 6))
-    state[0,0] = np.random.choice(np.linspace(-7, 7, 20))
-    state[0,1] = np.random.choice(np.linspace(-7, 7, 20))
-    state[0,2] = np.random.choice(np.linspace(-np.pi/3.0, np.pi/3.0, 20))
+    quad_state = np.zeros((steps, 6))
+    quad_state[0,0] = 0.0 #np.random.choice(np.linspace(-5, 5, 20))
+    quad_state[0,1] = 0.0 #np.random.choice(np.linspace(-12, 12, 20))
+    quad_state[0,2] = 0.0 #np.random.choice(np.linspace(-np.pi/3.0, np.pi/3.0, 20))
+
+    ball_state = np.zeros((steps, 4))
+    ball_state[0, 0] = 0.0 #4.0
+    ball_state[0, 1] = 10.0 #-1.0
+    ball_state[0, 2] = 0.0 #-2.0
+    ball_state[0, 3] = 0.0 #15.0
 
     lastErr_x = 0.0
     lastErr_y = 0.0
@@ -137,25 +152,27 @@ if __name__ == "__main__":
     desired_angle = 0.0
 
     for i in range(0,steps-1):
-        error_x = state[i,0] - 0.0
+        contact = (np.linalg.norm(ball_state[i,0:2] - quad_state[i,0:2]) < 0.5)
+        ball_state[i+1,:] = ball.step_dynamics(dt, ball_state[i,:], contact)
+        error_x = quad_state[i,0] - 0.0
         dE_x = (error_x - lastErr_x) / dt
         limit = min(0.45 * abs(error_x)/5.0, 0.25)
         desired_angle = max(min(10000.0*error_x+ 10000.0*dE_x, limit), -limit) 
-        error_y = 0.0 - state[i,1]
-        error_th = desired_angle - state[i,2]
+        error_y = 0.0 - quad_state[i,1]
+        error_th = desired_angle - quad_state[i,2]
         dE_y = (error_y - lastErr_y) / dt
         dE_th = (error_th - lastErr_th) / dt
         force_command = 0.1*error_y + 0.3*dE_y + 0.981
         roll_command = 4.0*error_th + 1.5*dE_th
         input_u = np.array([force_command, roll_command])
-        state[i+1,:] = quad.step_dynamics(dt, state[i,:], input_u)
+        quad_state[i+1,:] = quad.step_dynamics(dt, quad_state[i,:], input_u)
         lastErr_y = error_y
         lastErr_th = error_th
         lastErr_x = error_x
 
-    state = np.reshape(state[:,0:3], (steps, 3))
+    quad_state = np.reshape(quad_state[:,0:3], (steps, 3))
 
-    a = Animator(time, state)
+    a = Animator(time, quad_state, ball_state)
 
 
 
