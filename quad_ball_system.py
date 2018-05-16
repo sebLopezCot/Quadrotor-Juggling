dcdfed202bc8960c5365c2eb7a31d24424f71952
Sebastian Lopez-Cot <slcot@mit.edu>
2018-05-16 01:50:21 -0400

    Video generation and realized that tanh will not work

--- quad_ball_system.py
+++ quad_ball_system.py
@@ -5,8 +5,13 @@ import matplotlib.animation as animation
 import numpy as np
 import scipy as sp
 
-from pydrake.all import MathematicalProgram
+import matplotlib
+from matplotlib.patches import Rectangle, Circle
+
+from pydrake.all import MathematicalProgram, SolverType
 from pydrake.symbolic import (sin, cos, tanh)
+
+from IPython.display import HTML
 
 def two_norm(x):
     slack = .001
@@ -137,7 +142,7 @@ class BallQuadSystem(object):
         
         for j in range(4):
             mp.AddLinearConstraint(ball_q[0][j] == ball_start_q[j])
-            if j < 3:
+            if j < 2:
                 mp.AddLinearConstraint(ball_q[-1][j] == ball_final_q[j])
                 
 
@@ -154,6 +159,8 @@ class BallQuadSystem(object):
         # Solve the optimization
         print "Number of decision vars: ", mp.num_vars()
 
+        mp.SetSolverOption(SolverType.kSnopt, "Major iterations limit", 100000)
+
         print "Solve: ", mp.Solve()
 
         quad_traj = mp.GetSolution(quad_q)
@@ -162,11 +169,124 @@ class BallQuadSystem(object):
 
         return (quad_traj, ball_traj, input_traj, time_array)
 
-class SystemVisualizer(object):
+class Quadrotor(object):
+    
+    def __init__(self):
+        self.m = 0.1
+        self.l = 0.05
+        self.g = -9.81
+        self.g_vec = np.array([0.0, self.g])
+        self.max_f = 10.0
+        self.min_f = 0.0
+        self.max_roll = 1.0
+        self.min_roll = -1.0
+        self.restitution = 1.0
+
+    def step_dynamics(self, t, state, u):
+        a_f = u[0] * 1.0 / self.m
+        r_ddot = u[1]
+        norm = np.array([-np.sin(state[2]), np.cos(state[2])])
+        pos_ddot = norm * a_f + self.g_vec 
+        
+        state[0] += state[3]*t + 0.5*pos_ddot[0]*t**2.0
+        state[1] += state[4]*t + 0.5*pos_ddot[1]*t**2.0
+        state[2] += state[5]*t + 0.5*r_ddot*t**2.0
+        state[3] += pos_ddot[0]*t
+        state[4] += pos_ddot[1]*t
+        state[5] += r_ddot*t
+
+        return state
+
+
+class Ball(object):
 
     def __init__(self):
-        pass
+        self.m = 0.001
+        self.g = -9.81
 
-    def visualize(self):
-        pass
+    def step_dynamics(self, t, state, contact):
+        if contact:
+            state[3] = -state[3]*0.9
+        
+        state[0] += state[2]*t
+        state[1] += state[3]*t + 0.5*self.g*t**2.0
+        state[2] += 0.0
+        state[3] += self.g*t
+
+        return state
+
+class Animator(object):
+
+    def __init__(self, t, quad_states, ball_states):
+        self.t = t
+        self.quad_states = quad_states
+        self.ball_states = ball_states
+        
+        # first set up the figure, the axis, and the plot elements we want to animate
+        fig = plt.figure()
+        
+        # some dimesions
+        self.cart_width = 0.4
+        self.cart_height = 0.05
+        
+        # set the limits based on the motion
+        self.xmin = -10 #np.around(quad_states[:, 0].min() - self.cart_width / 2.0, 1)
+        self.xmax = 10 #np.around(quad_states[:, 0].max() + self.cart_width / 2.0, 1)
+        
+        # create the axes
+        self.ax = plt.axes(xlim=(self.xmin, self.xmax), ylim=(-1, 10), aspect='equal')
+        
+        # display the current time
+        self.time_text = self.ax.text(0.04, 0.9, '', transform=self.ax.transAxes)
+        
+        # create a rectangular cart
+        self.ball = Rectangle([ball_states[0,0], ball_states[0,1]], 0.1, 0.1)
+        self.rect = Rectangle([quad_states[0, 0] - self.cart_width / 2.0, quad_states[0,0]-self.cart_height / 2],
+            self.cart_width, self.cart_height, fill=True, color='red', ec='black')
+        self.rect_2 = Rectangle([quad_states[0,0] - self.cart_width / 2.0, quad_states[0,0]-self.cart_height / 2],
+                0.05, 0.05, fill=True, color='blue', ec='black')
+        self.rect_3 = Rectangle([quad_states[0,0] + self.cart_width / 2.0 - 0.05, quad_states[0,0]-self.cart_height / 2],
+                0.05, 0.05, fill=True, color='blue', ec='black')
+        self.ax.add_patch(self.rect)
+        self.ax.add_patch(self.rect_2)
+        self.ax.add_patch(self.rect_3)
+        self.ax.add_patch(self.ball)
+
+        # call the animator function
+        self.anim = animation.FuncAnimation(fig, self.animate, frames=len(t), init_func=self.init_anim,
+                interval=t[-1] / len(t) * 1000, blit=True, repeat=False)
+        
+        # save the animation if a filename is given
+        #if filename is not None:
+        self.anim.save('filename.mp4', fps=30, codec='libx264')
+
+        #plt.show()
+
+    def gen_html_animation(self):
+        pass #HTML(self.anim.to_html5_video())
+
+    def init_anim(self):
+        self.time_text.set_text('')
+        self.rect.set_xy((0.0, 0.0))
+        self.rect_2.set_xy((0.0, 0.0))
+        self.rect_3.set_xy((0.0, 0.0))
+        self.ball.set_xy((0.0, 0.0))
+        return self.time_text, self.rect, self.rect_2, self.rect_3, self.ball
+
+    # animation function: update the objects
+    def animate(self, i):
+        self.time_text.set_text('time = {:2.2f}'.format(self.t[i]))
+        
+        self.ball.set_xy((self.ball_states[i,0], self.ball_states[i,1]))
+        self.rect.set_xy((self.quad_states[i, 0] - self.cart_width / 2.0, self.quad_states[i,1]-self.cart_height / 2))
+        self.rect_2.set_xy((self.quad_states[i,0] - self.cart_width / 2.0, self.quad_states[i,1]))
+        self.rect_3.set_xy((self.quad_states[i,0] + self.cart_width / 2.0 - 0.05, self.quad_states[i,1]))
+
+        t = matplotlib.transforms.Affine2D().rotate_around(self.quad_states[i, 0], self.quad_states[i, 1], self.quad_states[i, 2])
+
+        self.rect.set_transform(t + plt.gca().transData)
+        self.rect_2.set_transform(t + plt.gca().transData)
+        self.rect_3.set_transform(t + plt.gca().transData)
+
+        return self.time_text, self.rect, self.rect_2, self.rect_3, self.ball
 
